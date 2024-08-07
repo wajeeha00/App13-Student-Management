91e5960d77479f57353ea263e488111de7f76ad7
wajeeha00 <wajeehaaftab7890@gmail.com>
2024-08-07 11:16:24 +0500

    fsgbxsfrhbsf

--- example.py
+++ example.py
@@ -1,37 +1,82 @@
-from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QFormLayout, QComboBox, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QMessageBox
+from PyQt6.QtGui import QAction,QIcon
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton , \
+QFormLayout, QComboBox, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QMessageBox, QToolBar, QStatusBar
 import sys
 import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Student Management System')
-        file_menu_item = self.menuBar().addMenu('&File')
-        help_menu_item = self.menuBar().addMenu('&Help')
-        edit_menu_item = self.menuBar().addMenu('&Edit')
+        self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800, 600)
 
-        add_student_action = QAction('Add Student', self)
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+        edit_menu_item = self.menuBar().addMenu("&Edit")
+
+        add_student_action = QAction("Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
-        search_student_action = QAction('Search Student', self)
-        search_student_action.triggered.connect(self.search)
-        edit_menu_item.addAction(search_student_action)
-
-        about_action = QAction('About', self)
+        about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
+        search_action = QAction("Search", self)
+        edit_menu_item.addAction(search_action)
+        search_action.triggered.connect(self.search)
+
         self.table = QTableWidget()
         self.table.setColumnCount(4)
-        self.table.setHorizontalHeaderLabels(['ID', 'Name', 'Course', 'Mobile'])
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+        toolbar.action = QAction(QIcon("icons/add.png"),"Add Student", self)
+        toolbar.action.triggered.connect(self.insert)
+        toolbar.addAction(toolbar.action)
+
+        toolbar.action = QAction(QIcon("icons/search.png"),"Search", self)
+        toolbar.action.triggered.connect(self.search)
+        toolbar.addAction(toolbar.action)
+
+        #create status bar
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+        #detect a cell click
+        self.table.cellClicked.connect(self.cell_clicked)
+
+    def cell_clicked(self):
+        edit_button = QPushButton('Edit Record')
+        edit_button.clicked.connect(self.edit)
+
+       
+
+        delete_button = QPushButton('delete Record')
+        delete_button.clicked.connect(self.delete)
+
+        children = self.statusbar.findChildren(QPushButton)
+        if children:
+            for child in children:
+                self.statusbar.removeWidget(child)
+        self.statusbar.addWidget(edit_button)
+        self.statusbar.addWidget(delete_button)
+
+    def delete(self):
+        dialog = DeleteDialog()
+        dialog.exec()
+    
+    def edit(self):
+        dialog = EditDialog()
+        dialog.exec()
+
     def load_data(self):
-        connection = sqlite3.connect('database.db')
-        result = connection.execute('SELECT * FROM students')
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
         self.table.setRowCount(0)
         for row_number, row_data in enumerate(result):
             self.table.insertRow(row_number)
@@ -40,13 +85,18 @@ class MainWindow(QMainWindow):
         connection.close()
 
     def insert(self):
-        dialog = InsertDialog(self)
-        dialog.exec()
-    
-    def search(self):
-        dialog = SearchDialog(self)
+        dialog = InsertDialog()
         dialog.exec()
 
+    def search(self):
+        dialog = SearchDialog()
+        dialog.exec()
+
+class DeleteDialog(QDialog):
+    pass
+
+class EditDialog(QDialog):
+    pass
 
 class InsertDialog(QDialog):
     def __init__(self, parent=None):
