11f133fd0c57e93326d307d08c37deb6c98e1047
wajeeha00 <wajeehaaftab7890@gmail.com>
2024-08-07 12:09:02 +0500

    jgh

--- main.py
+++ main.py
@@ -1,39 +1,281 @@
-from PyQt6.QtWidgets import QApplication, QBoxLayout, QWidget, QLabel, QGridLayout, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QFormLayout, QComboBox
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction,QIcon
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton , \
+QFormLayout, QComboBox, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QMessageBox, QToolBar, QStatusBar
 import sys
-from datetime import datetime
-class AgeCalculator(QWidget):
+import sqlite3
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Age Calculator')
-        grid = QGridLayout()
-        name_label = QLabel('Enter your name:')
-        self.name_line_edit = QLineEdit()
-        date_birth_label = QLabel('Enter your Date of Birth MM/DD/YYYY:')
-        #make it instance variable - add self 
-        self.date_birth_line_edit = QLineEdit()
+        self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800, 600)
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+        edit_menu_item = self.menuBar().addMenu("&Edit")
+
         
-        calculate_button = QPushButton('Calculate')
-        calculate_button.clicked.connect(self.calculate_age)
-        self.output_label = QLabel('Your age is: ')
+        add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
+        file_menu_item.addAction(add_student_action)
 
-        grid.addWidget(name_label, 0, 0)
-        grid.addWidget(self.name_line_edit, 0, 1)
-        grid.addWidget(date_birth_label, 1, 0)
-        grid.addWidget(self.date_birth_line_edit, 1, 1)
-        grid.addWidget(calculate_button, 2, 0,1,2)
-        #span across 2 columns and in 3rd row
-        grid.addWidget(self.output_label, 3, 0, 1, 2)
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
+        about_action.triggered.connect(self.about)
 
-        self.setLayout(grid)
 
-    def calculate_age(self):
-        current_year = datetime.now().year
-        date_of_birth = self.date_birth_line_edit.text()
-        age = current_year - int(date_of_birth[-4:])
-        self.output_label.setText(f'{self.name_line_edit.text()} is {age} years old.')
+        search_action = QAction("Search", self)
+        edit_menu_item.addAction(search_action)
+        search_action.triggered.connect(self.search)
 
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
+
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+        toolbar.action = QAction(QIcon("icons/add.png"),"Add Student", self)
+        toolbar.action.triggered.connect(self.insert)
+        toolbar.addAction(toolbar.action)
+
+        toolbar.action = QAction(QIcon("icons/search.png"),"Search", self)
+        toolbar.action.triggered.connect(self.search)
+        toolbar.addAction(toolbar.action)
+
+        #create status bar
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+        #detect a cell click
+        self.table.cellClicked.connect(self.cell_clicked)
+
+    def cell_clicked(self):
+        edit_button = QPushButton('Edit Record')
+        edit_button.clicked.connect(self.edit)
+
+       
+
+        delete_button = QPushButton('delete Record')
+        delete_button.clicked.connect(self.delete)
+
+        children = self.statusbar.findChildren(QPushButton)
+        if children:
+            for child in children:
+                self.statusbar.removeWidget(child)
+        self.statusbar.addWidget(edit_button)
+        self.statusbar.addWidget(delete_button)
+    
+    def about(self):
+        dialog = AboutDialog()
+        dialog.exec()
+
+    def delete(self):
+        dialog = DeleteDialog()
+        dialog.exec()
+    
+    def edit(self):
+        dialog = EditDialog()
+        dialog.exec()
+
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+    def search(self):
+        dialog = SearchDialog()
+        dialog.exec()
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('About')
+        content = """ 
+        This app was created by Wajeeha Aftab by following the tutorial at Udemy.
+        """
+        self.setText(content)
+
+class DeleteDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Delete Student Data')
+
+        layout = QGridLayout()
+        confirmation = QLabel('Are you sure you want to delete this record?')
+        yes = QPushButton('YES')
+        no = QPushButton('NO')
+
+        layout.addWidget(confirmation, 0, 0, 1, 2)
+        layout.addWidget(yes, 1, 0)
+        layout.addWidget(no, 1, 1)
+        self.setLayout(layout)
+
+        yes.clicked.connect(self.delete_student)
+        no.clicked.connect(self.close)
+    
+   
+    
+    def delete_student(self):
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        cursor.execute('DELETE FROM students WHERE id = ?', (main_window.table.item(main_window.table.currentRow(), 0).text(),))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()
+        self.close()
+
+        confirmation = QMessageBox()
+        confirmation.setText('Record deleted successfully')
+
+        confirmation.exec()
+
+class EditDialog(QDialog):
+     def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Update Student Data')
+        self.setFixedWidth(300)
+        self.setFixedHeight(200)
+
+        layout = QVBoxLayout()
+
+        index = main_window.table.currentRow()
+        student_name = main_window.table.item(index, 1).text()
+        course_name = main_window.table.item(index, 2).text()
+        mobile = main_window.table.item(index, 3).text()
+
+
+        self.student_name = QLineEdit(self)
+        self.student_name.setText(student_name)
+        layout.addWidget(self.student_name)
+
+        self.course= QComboBox()
+        courses = ['Math', 'Astronomy', 'Physics', 'Biology']
+        self.course.addItems(courses)
+        self.course.setCurrentText(course_name)
+        layout.addWidget(self.course)
+
+        self.mobile = QLineEdit(self)
+        self.mobile.setText(mobile)
+        layout.addWidget(self.mobile)
+
+        self.button = QPushButton('Update', self)
+        self.button.clicked.connect(self.update_student)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+    
+     def update_student(self):
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        cursor.execute('UPDATE students SET name = ?, course = ?, mobile = ? WHERE id = ?', (self.student_name.text(), self.course.itemText(self.course.currentIndex()), self.mobile.text(), main_window.table.item(main_window.table.currentRow(), 0).text()))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        #refresh the table
+        main_window.load_data()
+
+
+class InsertDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Add Student Data')
+        self.setFixedWidth(300)
+        self.setFixedHeight(200)
+
+        layout = QVBoxLayout()
+
+        self.student_name = QLineEdit(self)
+        self.student_name.setPlaceholderText('Enter student name')
+        layout.addWidget(self.student_name)
+
+        self.course= QComboBox()
+        courses = ['Math', 'Science', 'History', 'Geography', 'Computer Science']
+        self.course.addItems(courses)
+        layout.addWidget(self.course)
+
+        self.mobile = QLineEdit(self)
+        self.mobile.setPlaceholderText('Enter mobile number')
+        layout.addWidget(self.mobile)
+
+        self.button = QPushButton('Register', self)
+        self.button.clicked.connect(self.add_student)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course.itemText(self.course.currentIndex())
+        mobile = self.mobile.text()
+
+        if name and course and mobile:
+            connection = sqlite3.connect('database.db')
+            cursor = connection.cursor()
+            cursor.execute('INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)', (name, course, mobile))
+            connection.commit()
+            cursor.close()
+            connection.close()
+
+            self.parent().load_data()
+            self.close()
+        else:
+            QMessageBox.warning(self, 'Error', 'Please fill in all fields')
+
+class SearchDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Search Student Data')
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        self.student_name = QLineEdit(self)
+        self.student_name.setPlaceholderText('Enter student name')
+        layout.addWidget(self.student_name)
+
+        self.button = QPushButton('Search', self)
+        self.button.clicked.connect(self.search_student)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def search_student(self):
+        name = self.student_name.text()
+
+        if name:
+            connection = sqlite3.connect('database.db')
+            cursor = connection.cursor()
+            cursor.execute('SELECT * FROM students WHERE name = ?', (name,))
+            result = cursor.fetchone()
+            cursor.close()
+            connection.close()
+
+            if result:
+                QMessageBox.information(self, 'Success', f'Name: {result[1]}\nCourse: {result[2]}\nMobile: {result[3]}')
+            else:
+                QMessageBox.warning(self, 'Error', 'Student not found')
+            self.close()
+        else:
+            QMessageBox.warning(self, 'Error', 'Please fill in all fields')
 
 app = QApplication(sys.argv)
-age_calculator = AgeCalculator()
-age_calculator.show()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
